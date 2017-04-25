8560d760ad2166cd7cb08ce554c1c78f8ff529a4
qingyuan <qingyuan.ke@foxmail.com>
2017-04-25 21:23:06 +0800

    新增AES文件加解密

---
+++ src/main/java/com/ftsafe/iccbd/utils/security/exception/CryptoException.java
@@ -0,0 +1,14 @@
+package com.ftsafe.iccbd.utils.security.exception;
+
+/**
+ * Created by ft on 2017/4/25.
+ */
+public class CryptoException extends Exception {
+
+    public CryptoException() {
+    }
+
+    public CryptoException(String message, Throwable throwable) {
+        super(message, throwable);
+    }
+}
---
+++ src/test/java/com/ftsafe/iccbd/utils/security/aes/AesTest.java
@@ -0,0 +1,31 @@
+package com.ftsafe.iccbd.utils.security.aes;
+
+import com.ftsafe.iccbd.utils.security.exception.CryptoException;
+import junit.framework.TestCase;
+
+import java.io.File;
+
+/**
+ * Created by ft on 2017/4/25.
+ */
+public class AesTest extends TestCase {
+    public void testEncrypt() throws Exception {
+        String key = "Mary has one cat";
+        File inputFile = new File("C:\\Users\\Public\\Pictures\\Sample Pictures\\Hydrangeas.jpg");
+        File encryptedFile = new File("document.encrypted.jpg");
+        File decryptedFile = new File("document.decrypted.jpg");
+
+        try {
+            Aes.encrypt(key, inputFile, encryptedFile);
+            Aes.decrypt(key, encryptedFile, decryptedFile);
+        } catch (CryptoException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        }
+    }
+
+    public void testDecrypt() throws Exception {
+
+    }
+
+}
